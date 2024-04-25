41df44802e4c7a6db911f1436836ae12f32a505c
Hassan_Ali <165322829+Hassanfarid6@users.noreply.github.com>
2024-04-25 21:50:34 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+ 
+import inquirer from "inquirer"
+
+let todos = []
+let condition = "ture"
+while(condition){
+let todoQuestion = await inquirer.prompt([
+    {
+        name: "firstQ1",
+        type: "input",
+        message: "What would you like to add in your todo?"
+    },
+    {
+        name: "secondQ2",
+        type: "confirm",
+        message: "Would you like to add more in your todo?",
+        default: "ture",
+    }
+    ])
+    todos.push(todoQuestion.firstQ1)
+    console.log(todos);
+    condition = todoQuestion.secondQ2
+    
+}
